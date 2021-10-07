3319b1e271bf4b3d9616c02cf1033f00124ad998
Felipe Patrício <86684357+felipepatricioo@users.noreply.github.com>
2021-10-07 19:31:34 -0300

    commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require("path");
+
+const app = express();
+const port = 4500
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+
+
+app.get("/delete", (req, res) => {
+    res.render("../views/delete");
+  });
+
+
+app.get("/create", (req, res) => {
+    res.render("../views/create");
+  });
+
+
+app.get("/update", (req, res) => {
+    res.render("../views/edit");
+  });
+
+
+app.get("/read", (req, res) => {
+    res.render("../views/details");
+  });
+
+
+app.get("/", (req, res) => {
+    res.render('index.ejs')
+})
+
+app.listen(port, () =>
+  console.log(`Server listening in http://localhost:${port}`));
