5f04f13d575b1a96cd3e19096d19e93380c4d047
Felipe Patrício <86684357+felipepatricioo@users.noreply.github.com>
2021-10-11 14:23:53 -0300

    teste

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require("path");
 
 const app = express();
-const port = 4500
+const port =  process.env.PORT || 3000
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
