63e5d9954eab798b3c4a8c4306575606b63c8280
Felipe Patrício <86684357+felipepatricioo@users.noreply.github.com>
2021-10-18 15:21:15 -0300

    heroku test

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # catalogProject
 
-The objective of this project was to create a music albums catalog where the user can catalog the albums he have listened to, we used Node.JS/Express and Postgresql integrated with Sequelize to our database.
+The objective of this project was to create a music album catalog where the user can catalog the albums he has listened to, we used Node.JS/Express and Postgresql integrated with Sequelize to our database.
--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-const express = require('express');
+const express = require("express");
 const path = require("path");
-require('dotenv').config();
+require("dotenv").config();
 const db = require("./models/database");
 const albums = require("./models/albums");
 
 const app = express();
-const port =  process.env.PORT;
+const port = process.env.PORT;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "/views/public")));
 app.use(express.urlencoded({ extended: true }));
 
-let message = ""
+let message = "";
 
 app.get("/create", (req, res) => {
-  
-  setTimeout(() => {    
-    message = "";  
+  setTimeout(() => {
+    message = "";
   }, 1000);
 
-    res.render("../views/create", {message: message});
-})
+  res.render("../views/create", { message: message });
+});
 
-app.post('/create', async (req,res) =>{
-  const { album_name, artist_name, release_date, album_cover} = req.body;
+app.post("/create", async (req, res) => {
+  const { album_name, artist_name, release_date, album_cover } = req.body;
 
   if (!artist_name) {
     res.render("../views/create", {
@@ -51,66 +50,95 @@ app.post('/create', async (req,res) =>{
 
   try {
     const albums2 = await albums.create({
-        album_name:album_name,
-        artist_name:artist_name,
-        release_date:release_date,
-        album_cover:album_cover,
+      album_name: album_name,
+      artist_name: artist_name,
+      release_date: release_date,
+      album_cover: album_cover,
     });
 
     message = "Album sucessfully updated!";
-    res.render("../views/create", {message: message});
-
+    res.render("../views/create", { message: message });
   } catch (err) {
     console.log(err);
 
     message = "Error: Album not registered";
-    res.render("../views/create", {message: message});
+    res.render("../views/create", { message: message });
+  }
+});
+
+app.get("/read", async (req, res) => {
+  const albums1 = await albums.findAll();
+  res.render("../views/read", { albums1: albums1 });
+});
+
+app.get("/update/:id", async (req, res) => {
+  const albums4 = await albums.findByPk(req.params.id);
+  res.render("../views/update", { albums4: albums4 });
+});
+
+app.post("/update/:id", async (req, res) => {
+  const albums4 = await albums.findByPk(req.params.id);
+  const { album_name, artist_name, release_date, album_cover } = req.body;
+
+  if (!artist_name) {
+    res.render("../views/update", {
+      albums4: albums4,
+      message: "Artist Name is mandatory",
+    });
   }
 
+  if (!album_name) {
+    res.render("../views/update", {
+      albums4: albums4,
+      message: "Album Name is mandatory",
+    });
+  }
+
+  if (!album_cover) {
+    res.render("../views/update", {
+      albums4: albums4,
+      message: "Album Cover is mandatory",
+    });
+  }
+
+  if (!release_date) {
+    res.render("../views/update", {
+      albums4: albums4,
+      message: "Release Date is mandatory",
+    });
+  }
+
+  try {
+    albums4.album_name = album_name;
+    albums4.artist_name = artist_name;
+    albums4.release_date = release_date;
+    albums4.album_cover = album_cover;
+
+    await albums4.save();
+
+    message = "Album sucessfully updated!";
+    res.redirect("/read");
+  } catch (err) {
+    console.log(err);
+
+    message = "Error: Album not updated";
+    res.render("../views/update", { message: message });
+  }
 });
 
-
-
-app.get('/read', async (req,res) => {
-  const albums1 = await albums.findAll();
-  res.render("../views/read", {albums1: albums1});
-});
-
-app.get('/update/:id', async (req,res) => {
-  const albums4 = await albums.findByPk(req.params.id);
-  res.render("../views/update", {albums4: albums4});
-});
-
-
-app.post('/update/:id', async (req,res) =>{
-  const albums4 = await albums.findByPk(req.params.id);
-  const { album_name, artist_name, release_date, album_cover} = req.body;
-
-  albums4.album_name = album_name;
-  albums4.artist_name = artist_name;
-  albums4.release_date = release_date;
-  albums4.album_cover = album_cover;
-
-  await albums4.save();
-
-  res.redirect("/read");
-
-});
-
-
-app.get('/delete/:id', async (req,res) => {
+app.get("/delete/:id", async (req, res) => {
   const albums5 = await albums.findByPk(req.params.id);
 
   await albums5.destroy();
 
   res.redirect("/read");
-
 });
 
 app.get("/", (req, res) => {
-  res.render('index.ejs')
-})
+  res.render("index.ejs");
+});
 
-db.connected()
+db.connected();
 app.listen(port, () =>
-  console.log(`Server listening in http://localhost:${port}`));
+  console.log(`Server listening in http://localhost:${port}`)
+);
