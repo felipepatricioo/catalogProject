8add701a378cc18fa8951a96ce2ee8e9adb53690
Felipe Patrício <86684357+felipepatricioo@users.noreply.github.com>
2021-10-18 22:30:13 -0300

    updating dialect options

--- models/database/index.js
+++ models/database/index.js
@@ -3,7 +3,13 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER,process.env.DB_PASS, {
   host:  process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialect: "postgres"
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false 
+    }
+  },
 });
 
 async function connected(){
@@ -11,7 +17,7 @@ async function connected(){
         await sequelize.authenticate();
         console.log('Connected to database');
     }catch (error){
-        console.log('Error connecting to database');
+        console.log('Error connecting to database', error);
     }
 }
 
